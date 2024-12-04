76cdea04f8f57b961e5b3919c04a72a8fd41ccb5
Harpreetsingh-Developer <143pajihacker@gmail.com>
2024-12-05 00:35:30 +0530

    times

--- main.py
+++ main.py
@@ -1,1 +1,10 @@
 import requests
+
+def FetchAndSavetoFile(url ,path ):
+    r = requests.get(url)
+    with open (path,"w", encoding='utf-8') as f:
+        f.write(r.text)
+
+url = "https://timesofindia.indiatimes.com/city/delhi"
+
+FetchAndSavetoFile(url,"data/times.html")
